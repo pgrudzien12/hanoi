320c532a3d3d2b807195e46c75303a7015c158f2
Pawel Grudzien <p.grudzien12@gmail.com>
2016-11-06 00:09:17 +0100

    moved hanoi to new file

---
+++ Hanoi/Hanoi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    public class Hanoi
+    {
+        Stake _left;
+        Stake _center;
+        Stake _right;
+        private readonly int _biggestRing;
+
+        public Hanoi(int biggestRing)
+        {
+            _biggestRing = biggestRing;
+            _left = new Stake(biggestRing);
+            _center = new Stake(0);
+            _right = new Stake(0);
+        }
+
+        public void Solve()
+        {
+            _left.Move(_left.Biggest, _right, _center);
+        }
+
+        public bool IsSolved()
+        {
+            return _left.Count == 0 && _center.Count == 0 && _right.Biggest.Size == _biggestRing && _right.Validate();
+        }
+    }
+}
--- Hanoi/Program.cs
+++ Hanoi/Program.cs
@@ -30,29 +30,4 @@ namespace Hanoi
             tower.Solve();
         }
     }
-    public class Hanoi
-    {
-        Stake _left;
-        Stake _center;
-        Stake _right;
-        private readonly int _biggestRing;
-
-        public Hanoi(int biggestRing)
-        {
-            _biggestRing = biggestRing;
-            _left = new Stake(biggestRing);
-            _center = new Stake(0);
-            _right = new Stake(0);
-        }
-
-        public void Solve()
-        {
-            _left.Move(_left.Biggest, _right, _center);
-        }
-
-        public bool IsSolved()
-        {
-            return _left.Count == 0 && _center.Count == 0 && _right.Biggest.Size == _biggestRing && _right.Validate();
-        }
-    }
 }
